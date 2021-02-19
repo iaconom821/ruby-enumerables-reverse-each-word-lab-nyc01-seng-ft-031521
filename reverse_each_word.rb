987c7aa32067d61ab2c6bde6d0cbc718d49a4988
Michael Iacono <iaconom821@gmail.com>
2021-02-19 16:36:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
 def reverse_each_word(a_string)
   string_array = a_string.split(" ")
-  string_array.each do |val| val.reverse end
+  new_string = ''
+  string_array.each do |val| 
+    new_string += val.reverse 
+    end
+  new_string
 end 
